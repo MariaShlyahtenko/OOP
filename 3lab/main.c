33fa8c64340022a809af44c5b51f50362b5de9ae
Maria <ms.shlyahtenko@gamil.com>
2021-10-01 13:19:41 +0700

    done

--- 3lab/main.c
+++ 3lab/main.c
@@ -12,20 +12,24 @@ class points
 private:
     int x;
     int y;
-    int directionx;
-    int directiony;
+    int directionx = 0;
+    int directiony = 0;
     int BreakPoint;
     int r;
     int g;
+    int c = 0;
     int b;
 
 public:
     void input()
     {
+        BreakPoint = rand() % 500;
 
         x = rand() % 400;
         y = rand() % 400;
+
         directionx = -1 + rand() % (1 - -1 + 1);
+
         directiony = -1 + rand() % (1 - -1 + 1);
         r = rand() % 256;
         g = rand() % 256;
@@ -78,14 +82,39 @@ public:
         y += directiony;
     }
 
-    int RandomMove()
+    void RandomMove()
     {
 
-        BreakPoint = 500;
-        directionx = -1 + rand() % (1 - -1 + 1);
-        directiony = -1 + rand() % (1 - -1 + 1);
+        while ((directionx == 0) && (directiony == 0))
+        {
 
-        return BreakPoint;
+            directionx = -1 + rand() % (1 - -1 + 1);
+            directiony = -1 + rand() % (1 - -1 + 1);
+        }
+
+        if ((x == 400) || (x == 0))
+        {
+            directionx *= -1;
+            directiony = -1 + rand() % 3;
+        }
+
+        if ((y == 400) || (y == 0))
+        {
+            directiony *= -1;
+            directionx = -1 + rand() % 3;
+        }
+        if (c == BreakPoint)
+        {
+
+            directiony = -1 + rand() % 3;
+            directionx = -1 + rand() % 3;
+
+            c = 0;
+        }
+        c++;
+        x += directionx;
+
+        y += directiony;
     }
 };
 
@@ -97,9 +126,9 @@ int main()
 {
 
     srand(time(NULL));
-    RenderPoints();
+    // RenderPoints();
     //PointsMove();
-    //RandPointMove();
+    RandPointMove();
     return 0;
 }
 
@@ -156,7 +185,7 @@ void PointsMove()
     MPoint.input();
     int r = rand() % 256, g = rand() % 256, b = rand() % 256;
     // Объект, который, собственно, является главным окном приложения
-    RenderWindow window(VideoMode(1920, 1080), "SFML Works!");
+    RenderWindow window(VideoMode(400, 400), "SFML Works!");
 
     // Главный цикл приложения. Выполняется, пока открыто окно
     while (window.isOpen())
@@ -194,11 +223,19 @@ void PointsMove()
 
 void RandPointMove()
 {
-    points MRPoint;
-    MRPoint.input();
+
+    points ArrPoint[100] = {};
+
+    int r = rand() % 256, g = rand() % 256, b = rand() % 256;
+
+    for (int i = 0; i < 100; i++)
+    {
+        //r = rand() % 256, g = rand() % 256, b = rand() % 256;
+        ArrPoint[i].input();
+    }
 
     // Объект, который, собственно, является главным окном приложения
-    RenderWindow window(VideoMode(1920, 1080), "SFML Works!");
+    RenderWindow window(VideoMode(400, 400), "SFML Works!");
 
     // Главный цикл приложения. Выполняется, пока открыто окно
     while (window.isOpen())
@@ -207,34 +244,28 @@ void RandPointMove()
         Event event;
         while (window.pollEvent(event))
         {
-            //  закрыть окно
+            // Пользователь нажал на «крестик» и хочет закрыть окно?
             if (event.type == Event::Closed)
+                // тогда закрываем его
                 window.close();
         }
 
-        int BreakPoint = MRPoint.RandomMove();
-        for (int i = 0; i < BreakPoint; i++)
+        window.clear(Color(0, 0, 0, 0));
+
+        for (int i = 0; i < 100; i++)
         {
-            int r = rand() % 256, g = rand() % 256, b = rand() % 256;
-            window.clear(Color(0, 0, 0, 0));
 
-            // Создаем фигуру - круг радиусом 3
             CircleShape circle(3.f);
+            circle.setFillColor(Color(ArrPoint[i].getr(), ArrPoint[i].getg(), ArrPoint[i].getrb()));
 
-            // Закрашиваем наш круг
-            circle.setFillColor(Color(r, g, b));
+            ArrPoint[i].RandomMove();
+            circle.move((ArrPoint[i].getX()), (ArrPoint[i].getY()));
 
-            circle.move((MRPoint.getX()), (MRPoint.getY()));
-
-            // Отрисовка круга
             window.draw(circle);
-
-            // Отрисовка окна
-            window.display();
-
-            sleep(milliseconds(1000 / 500));
-
-            MRPoint.LineMove();
         }
+
+        // Отрисовка окна
+        window.display();
+        sleep(milliseconds(1000 / 500));
     }
 }
