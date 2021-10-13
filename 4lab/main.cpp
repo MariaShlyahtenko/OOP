b36cb04500e265c4c0dd4ff9e01889a7f12cd922
Maria <ms.shlyahtenko@gamil.com>
2021-10-13 20:49:58 +0700

    f

--- 4lab/main.cpp
+++ 4lab/main.cpp
@@ -7,9 +7,9 @@
 using namespace sf;
 using namespace std;
 
-class points
+class figure
 {
-private:
+protected:
     int x;
     int y;
     int directionx = 0;
@@ -19,11 +19,12 @@ private:
     int g;
     int c = 0;
     int b;
+    int n = 0;
+    int m = 0;
 
 public:
     void input()
     {
-        BreakPoint = rand() % 500;
 
         x = rand() % 240;
         y = rand() % 240;
@@ -35,11 +36,16 @@ public:
         g = rand() % 256;
         b = rand() % 256;
     }
-    int rad()
+    void LineMove()
     {
-        int rad;
-        rad = 50;
-        return rad;
+        while ((directionx == 0) && (directiony == 0))
+        {
+            directionx = -1 + rand() % (1 - -1 + 1);
+            directiony = -1 + rand() % (1 - -1 + 1);
+        }
+        x += directionx;
+
+        y += directiony;
     }
 
     int getX()
@@ -63,29 +69,49 @@ public:
     {
         return b;
     }
-    void LineMoveline()
+    int getrm(int m)
     {
-        while ((directionx == 0) && (directiony == 0))
-        {
-            directionx = -1 + rand() % (1 - -1 + 1);
-            directiony = -1 + rand() % (1 - -1 + 1);
-        }
+        return m;
+    }
+    int getrn(int n)
+    {
+        return n;
+    }
+};
+class point : public figure
+{
 
-        if ((x == 0) || (x + 50 == 400))
+public:
+    void move()
+    {
+
+        if ((x == 0) || (x + n == 400))
         {
             directionx *= -1;
             directiony = -1 + rand() % 3;
         }
 
-        if ((y == 0) || (y + 2 == 400))
+        if ((y == 0) || (y + m == 400))
         {
             directiony *= -1;
             directionx = -1 + rand() % 3;
         }
-        x += directionx;
-
-        y += directiony;
     }
+};
+class line : public point
+{
+
+public:
+    void setn(int n)
+    {
+        this->n = n;
+    }
+    void setm(int m)
+    {
+        this->m = m;
+    }
+};
+/*
     void LineMove()
     {
         while ((directionx == 0) && (directiony == 0))
@@ -109,8 +135,7 @@ public:
 
         y += directiony;
     }
-    void LineMovetromb()
-    {
+    void LineMovetromb() {
         while ((directionx == 0) && (directiony == 0))
         {
             directionx = -1 + rand() % (1 - -1 + 1);
@@ -132,7 +157,7 @@ public:
 
         y += directiony;
     }
-
+    
     void RandomMove()
     {
 
@@ -167,7 +192,7 @@ public:
 
         y += directiony;
     }
-};
+};*/
 
 void RandPointMove();
 void RenderPoints();
@@ -179,199 +204,18 @@ int main()
 {
 
     srand(time(NULL));
-    //RenderPoints();
-    RenderTriangle();
+    // RenderPoints();
+    // RenderTriangle();
 
-    //RenderLine();
+    RenderLine();
     //PointsMove();
     // RandPointMove();
     return 0;
 }
-void RenderLine()
-{
-
-    points ArrPoint[100] = {};
-
-    int r = rand() % 256, g = rand() % 256, b = rand() % 256;
-
-    for (int i = 0; i < 100; i++)
-    {
-        //r = rand() % 256, g = rand() % 256, b = rand() % 256;
-        ArrPoint[i].input();
-    }
-
-    // Объект, который, собственно, является главным окном приложения
-    RenderWindow window(VideoMode(400, 400), "SFML Works!");
-
-    // Главный цикл приложения. Выполняется, пока открыто окно
-    while (window.isOpen())
-    {
-        // Обрабатываем очередь событий в цикле
-        Event event;
-        while (window.pollEvent(event))
-        {
-            // Пользователь нажал на «крестик» и хочет закрыть окно?
-            if (event.type == Event::Closed)
-                // тогда закрываем его
-                window.close();
-        }
-
-        window.clear(Color(0, 0, 0, 0));
-
-        for (int i = 0; i < 100; i++)
-        {
-
-            RectangleShape line(Vector2f(50, 2));
-            line.setFillColor(Color(ArrPoint[i].getr(), ArrPoint[i].getg(), ArrPoint[i].getrb()));
-            line.move((ArrPoint[i].getX()), (ArrPoint[i].getY()));
-            ArrPoint[i].LineMoveline();
-            window.draw(line);
-        }
-
-        // Отрисовка окна
-        window.display();
-        sleep(milliseconds(1000 / 500));
-    }
-}
-void RenderTriangle()
-{
-
-    points ArrPoint[100] = {};
-
-    int r = rand() % 256, g = rand() % 256, b = rand() % 256;
-
-    for (int i = 0; i < 100; i++)
-    {
-        //r = rand() % 256, g = rand() % 256, b = rand() % 256;
-        ArrPoint[i].input();
-    }
-
-    // Объект, который, собственно, является главным окном приложения
-    RenderWindow window(VideoMode(400, 400), "SFML Works!");
-
-    // Главный цикл приложения. Выполняется, пока открыто окно
-    while (window.isOpen())
-    {
-        // Обрабатываем очередь событий в цикле
-        Event event;
-        while (window.pollEvent(event))
-        {
-            // Пользователь нажал на «крестик» и хочет закрыть окно?
-            if (event.type == Event::Closed)
-                // тогда закрываем его
-                window.close();
-        }
-
-        window.clear(Color(0, 0, 0, 0));
-
-        for (int i = 0; i < 2; i++)
-        {
-
-            CircleShape romb(80, 3);
-            romb.setFillColor(Color(ArrPoint[i].getr(), ArrPoint[i].getg(), ArrPoint[i].getrb()));
-            romb.move((ArrPoint[i].getX()), (ArrPoint[i].getY()));
-            ArrPoint[i].LineMovetromb();
-            window.draw(romb);
-        }
-
-        // Отрисовка окна
-        window.display();
-        sleep(milliseconds(1000 / 500));
-    }
-}
 void RenderPoints()
 {
 
-    points ArrPoint[100] = {};
-
-    int r = rand() % 256, g = rand() % 256, b = rand() % 256;
-
-    for (int i = 0; i < 100; i++)
-    {
-        //r = rand() % 256, g = rand() % 256, b = rand() % 256;
-        ArrPoint[i].input();
-    }
-
-    // Объект, который, собственно, является главным окном приложения
-    RenderWindow window(VideoMode(400, 400), "SFML Works!");
-
-    // Главный цикл приложения. Выполняется, пока открыто окно
-    while (window.isOpen())
-    {
-        // Обрабатываем очередь событий в цикле
-        Event event;
-        while (window.pollEvent(event))
-        {
-            // Пользователь нажал на «крестик» и хочет закрыть окно?
-            if (event.type == Event::Closed)
-                // тогда закрываем его
-                window.close();
-        }
-
-        window.clear(Color(0, 0, 0, 0));
-
-        for (int i = 0; i < 2; i++)
-        {
-
-            CircleShape circle(30.f);
-            circle.setFillColor(Color(ArrPoint[i].getr(), ArrPoint[i].getg(), ArrPoint[i].getrb()));
-            circle.move((ArrPoint[i].getX()), (ArrPoint[i].getY()));
-            ArrPoint[i].LineMove();
-            window.draw(circle);
-        }
-
-        // Отрисовка окна
-        window.display();
-        sleep(milliseconds(1000 / 500));
-    }
-}
-
-void PointsMove()
-{
-    points MPoint;
-    MPoint.input();
-    int r = rand() % 256, g = rand() % 256, b = rand() % 256;
-    // Объект, который, собственно, является главным окном приложения
-    RenderWindow window(VideoMode(400, 400), "SFML Works!");
-
-    // Главный цикл приложения. Выполняется, пока открыто окно
-    while (window.isOpen())
-    {
-        // Обрабатываем очередь событий в цикле
-        Event event;
-        while (window.pollEvent(event))
-        {
-            //  закрыть окно
-            if (event.type == Event::Closed)
-                window.close();
-        }
-
-        window.clear(Color(0, 0, 0, 0));
-
-        // Создаем фигуру - круг радиусом 3
-        CircleShape circle(3.f);
-
-        // Закрашиваем наш круг
-        circle.setFillColor(Color(r, g, b));
-
-        circle.move((MPoint.getX()), (MPoint.getY()));
-
-        // Отрисовка круга
-        window.draw(circle);
-
-        // Отрисовка окна
-        window.display();
-
-        sleep(milliseconds(1000 / 500));
-
-        MPoint.LineMove();
-    }
-}
-
-void RandPointMove()
-{
-
-    points ArrPoint[100] = {};
+    point ArrPoint[100] = {};
 
     int r = rand() % 256, g = rand() % 256, b = rand() % 256;
 
@@ -404,11 +248,59 @@ void RandPointMove()
 
             CircleShape circle(3.f);
             circle.setFillColor(Color(ArrPoint[i].getr(), ArrPoint[i].getg(), ArrPoint[i].getrb()));
-
-            ArrPoint[i].RandomMove();
             circle.move((ArrPoint[i].getX()), (ArrPoint[i].getY()));
-
+            ArrPoint[i].move();
+            ArrPoint[i].LineMove();
             window.draw(circle);
+        }
+
+        // Отрисовка окна
+        window.display();
+        sleep(milliseconds(1000 / 500));
+    }
+}
+void RenderLine()
+{
+
+    line ArrPoint[100] = {};
+
+    int r = rand() % 256, g = rand() % 256, b = rand() % 256;
+
+    for (int i = 0; i < 100; i++)
+    {
+        //r = rand() % 256, g = rand() % 256, b = rand() % 256;
+        ArrPoint[i].input();
+        ArrPoint[i].setn(60);
+        ArrPoint[i].setm(2);
+    }
+
+    // Объект, который, собственно, является главным окном приложения
+    RenderWindow window(VideoMode(400, 400), "SFML Works!");
+
+    // Главный цикл приложения. Выполняется, пока открыто окно
+    while (window.isOpen())
+    {
+        // Обрабатываем очередь событий в цикле
+        Event event;
+        while (window.pollEvent(event))
+        {
+            // Пользователь нажал на «крестик» и хочет закрыть окно?
+            if (event.type == Event::Closed)
+                // тогда закрываем его
+                window.close();
+        }
+
+        window.clear(Color(0, 0, 0, 0));
+
+        for (int i = 0; i < 100; i++)
+        {
+
+            RectangleShape line(Vector2f(60, 2));
+            line.setFillColor(Color(ArrPoint[i].getr(), ArrPoint[i].getg(), ArrPoint[i].getrb()));
+            line.move((ArrPoint[i].getX()), (ArrPoint[i].getY()));
+            ArrPoint[i].move();
+            ArrPoint[i].LineMove();
+            window.draw(line);
         }
 
         // Отрисовка окна
